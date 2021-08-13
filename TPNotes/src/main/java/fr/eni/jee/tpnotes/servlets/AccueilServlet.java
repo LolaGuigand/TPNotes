097f4961db4ae9ba5d515f85fd8fbaf39634ebd1
lguigand2021 <lguigand2021@27SE21-25NZG5J.ad.campus-eni.fr>
2021-08-13 12:10:26 +0200

    Un peu plus stylé

--- TPNotes/src/main/java/fr/eni/jee/tpnotes/servlets/AccueilServlet.java
+++ TPNotes/src/main/java/fr/eni/jee/tpnotes/servlets/AccueilServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
  */
 
 
-@WebServlet("/")
+@WebServlet("")
 public class AccueilServlet extends HttpServlet {
    private static final long serialVersionUID = 1L;
       
